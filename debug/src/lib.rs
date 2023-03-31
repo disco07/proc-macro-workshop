5df5345bd1978a9182c71d31e2dcbff683e2c948
disco07 <koneenok@outlook.fr>
2023-03-31 23:21:00 +0200

    debug

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,8 +1,54 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{Attribute, Data, DataStruct, DeriveInput, Field, Fields, FieldsNamed, Meta, MetaNameValue, parse_macro_input, Path, PathSegment, Type};
 
-#[proc_macro_derive(CustomDebug)]
+#[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let ast = parse_macro_input!(input as DeriveInput);
+    // eprintln!("{:#?}", ast);
+    let name = ast.ident;
+    let name_string = format!("{}", name);
+    let fields = if let Data::Struct(DataStruct{fields: Fields::Named(FieldsNamed {named, ..}), ..}) = ast.data {
+        Some(named)
+    } else {
+        None
+    };
+    assert!(fields.is_some());
 
-    unimplemented!()
+    let binding = fields.unwrap();
+    let method = binding.iter().fold(vec![], |acc, f| {
+        let name = &f.ident.clone().unwrap();
+        let name_string = format!("{}", name);
+        let _ty = &f.ty;
+
+        quote! {
+            field(#name_string, &self.#name)
+        }
+        acc
+    });
+
+    let expanded = quote!{
+        impl std::fmt::Debug for #name {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                write!(f, "{} {{}}", #name_string, #method)
+            }
+        }
+    };
+
+    TokenStream::from(expanded)
+}
+
+fn get_attrs<'a>(field: &'a Field, attrs: &str) -> Option<&'a Attribute> {
+    while let Some(attr) = field.attrs.first() {
+        if let Meta::NameValue(MetaNameValue {path: Path {segments, ..}, ..}) = attr {
+            if let Some(PathSegment{ident, .. }) = segments.first() {
+                if ident != attrs {
+                    return None;
+                }
+
+                return Some(attr)
+            }
+        }
+    }
+    None
 }
