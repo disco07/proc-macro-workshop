99de66690fbb7a340723061a5d30164cfc3a8b40
disco07 <koneenok@outlook.fr>
2023-04-03 22:43:26 +0200

    derive debug

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,11 +1,11 @@
 use proc_macro::{TokenStream};
 use quote::quote;
-use syn::{Attribute, Ident, Data, DataStruct, DeriveInput, Expr, ExprLit, Field, Fields, FieldsNamed, Lit, Meta, MetaNameValue, parse_macro_input, Path, PathSegment, Generics, GenericParam, parse_quote};
+use syn::{Attribute, Ident, Data, DataStruct, DeriveInput, Expr, ExprLit, Field, Fields, FieldsNamed, Lit, Meta, MetaNameValue, parse_macro_input, Path, PathSegment, Generics, GenericParam, parse_quote, Type, TypePath, PathArguments, AngleBracketedGenericArguments, GenericArgument};
 
 #[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
-    eprintln!("{:#?}", ast);
+    // eprintln!("{:#?}", ast);
     let name = ast.ident;
     let name_string = format!("{}", name);
     let fields = if let Data::Struct(DataStruct{fields: Fields::Named(FieldsNamed {named, ..}), ..}) = ast.data {
@@ -15,7 +15,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
     };
     assert!(fields.is_some());
 
-    // Add a bound `T: HeapSize` to every type parameter T.
+    // Add a bound `T: std::fmt::Debug` to every type parameter T.
     let generics = add_trait_bounds(ast.generics);
     let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
 
@@ -27,7 +27,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
         create_field(f, name)
     });
 
-    let expanded = quote!{
+    let expanded = quote! {
         impl #impl_generics std::fmt::Debug for #name #ty_generics #where_clause {
             fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
                 f.debug_struct(#name_string).
@@ -36,7 +36,6 @@ pub fn derive(input: TokenStream) -> TokenStream {
             }
         }
     };
-
     TokenStream::from(expanded)
 }
 
@@ -58,6 +57,10 @@ fn get_attrs<'a>(field: &'a Field, attrs: &str) -> Option<&'a Attribute> {
 fn create_field(field: &Field, ident: &Ident) -> proc_macro2::TokenStream {
     let attr = get_attrs(field, "debug");
     let name_string = format!("{}", ident);
+    // let ty = ty_inner(&field.ty, "PhantomData");
+    // if ty.is_some() {
+    //     return proc_macro2::TokenStream::new()
+    // }
     if let Some(Attribute{meta, ..}) = attr {
         if let Meta::NameValue(MetaNameValue { value: Expr::Lit(ExprLit { lit, .. }), .. }) = meta {
             match lit {
@@ -71,6 +74,7 @@ fn create_field(field: &Field, ident: &Ident) -> proc_macro2::TokenStream {
             }
         }
     }
+
     quote! {
         field(#name_string, &self.#ident)
     }
@@ -85,3 +89,22 @@ fn add_trait_bounds(mut generics: Generics) -> Generics {
     }
     generics
 }
+
+// fn ty_inner<'a>(ty: &'a Type, wrapper: &str) -> Option<&'a Type> {
+//     if let Type::Path(TypePath {path: Path {segments, ..},..}) = ty {
+//         if let Some(PathSegment { ident, arguments }) = segments.iter().next() {
+//             if ident != wrapper {
+//                 return None;
+//             }
+//             if let PathArguments::AngleBracketed(AngleBracketedGenericArguments { args, .. }) =
+//                 arguments
+//             {
+//                 if let Some(GenericArgument::Type(ref inner_type)) = args.iter().next() {
+//                     return Some(inner_type);
+//                 }
+//             }
+//         }
+//     }
+//
+//     None
+// }
