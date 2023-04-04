bcc76e89731843067807d892eb71e623aee737e0
disco07 <koneenok@outlook.fr>
2023-04-04 16:58:26 +0200

    clippy

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -11,7 +11,6 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
     // eprintln!("{:#?}", ast);
     let name = ast.ident;
-    let name_string = format!("{}", name);
     let fields = if let Data::Struct(DataStruct {
         fields: Fields::Named(FieldsNamed { named, .. }),
         ..
@@ -26,9 +25,10 @@ pub fn derive(input: TokenStream) -> TokenStream {
     // Add a bound `T: std::fmt::Debug` to every type parameter T.
     let generics = add_trait_bounds(ast.generics);
     let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+    // let generic_types = ast.generics.type_params().map(|t|&t.ident).collect::<Vec<_>>();
 
     let binding = fields.unwrap();
-    let method = binding.iter().map(|f| {
+    let field = binding.iter().map(|f| {
         let name = &f.ident.clone().unwrap();
         let _ty = &f.ty;
 
@@ -38,8 +38,8 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let expanded = quote! {
         impl #impl_generics std::fmt::Debug for #name #ty_generics #where_clause {
             fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-                f.debug_struct(#name_string).
-                #(#method.)*
+                f.debug_struct(stringify!(#name)).
+                #(#field.)*
                 finish()
             }
         }
@@ -68,7 +68,6 @@ fn get_attrs<'a>(field: &'a Field, attrs: &str) -> Option<&'a Attribute> {
 
 fn create_field(field: &Field, ident: &Ident) -> proc_macro2::TokenStream {
     let attr = get_attrs(field, "debug");
-    let name_string = format!("{}", ident);
     // let ty = ty_inner(&field.ty, "PhantomData");
     // if ty.is_some() {
     //     return proc_macro2::TokenStream::new()
@@ -83,7 +82,7 @@ fn create_field(field: &Field, ident: &Ident) -> proc_macro2::TokenStream {
                 Lit::Str(s) => {
                     let t = s.token();
                     return quote! {
-                        field(#name_string, &format_args!(#t, self.#ident))
+                        field(stringify!(#ident), &format_args!(#t, self.#ident))
                     };
                 }
                 _ => unimplemented!(),
@@ -92,7 +91,7 @@ fn create_field(field: &Field, ident: &Ident) -> proc_macro2::TokenStream {
     }
 
     quote! {
-        field(#name_string, &self.#ident)
+        field(stringify!(#ident), &self.#ident)
     }
 }
 
@@ -100,7 +99,7 @@ fn create_field(field: &Field, ident: &Ident) -> proc_macro2::TokenStream {
 fn add_trait_bounds(mut generics: Generics) -> Generics {
     for param in &mut generics.params {
         if let GenericParam::Type(ref mut type_param) = *param {
-            type_param.bounds.push(parse_quote!(std::fmt::Debug));
+            type_param.bounds.push(parse_quote!(::std::fmt::Debug));
         }
     }
     generics
