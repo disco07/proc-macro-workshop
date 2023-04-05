ef23c63bfe7e44de270e4995a197e69c3dedf3a1
disco07 <koneenok@outlook.fr>
2023-04-06 00:09:03 +0200

    level 06

--- main.rs
+++ main.rs
@@ -6,29 +6,29 @@
 // To run the code:
 //     $ cargo run
 
-use derive_debug::CustomDebug;
-use std::fmt::Debug;
-
-pub trait Trait {
-    type Value;
-}
-
-#[derive(CustomDebug)]
-pub struct Field<T: Trait> {
-    values: Vec<T::Value>,
-}
-
-fn assert_debug<F: Debug>() {}
+// use derive_debug::CustomDebug;
+// use std::fmt::Debug;
+//
+// pub trait Trait {
+//     type Value;
+// }
+//
+// #[derive(CustomDebug)]
+// pub struct Field<T: Trait> {
+//     values: Vec<T::Value>,
+// }
+//
+// fn assert_debug<F: Debug>() {}
 
 fn main() {
-    // Does not implement Debug, but its associated type does.
-    struct Id;
-
-    impl Trait for Id {
-        type Value = u8;
-    }
-
-    assert_debug::<Field<Id>>();
+    // // Does not implement Debug, but its associated type does.
+    // struct Id;
+    //
+    // impl Trait for Id {
+    //     type Value = u8;
+    // }
+    //
+    // assert_debug::<Field<Id>>();
 }
 
 
