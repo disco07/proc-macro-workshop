ccaacf68c68f345285914cfc501bb4d22ef6642a
disco07 <koneenok@outlook.fr>
2023-04-06 15:15:26 +0200

    level 08

--- main.rs
+++ main.rs
@@ -6,34 +6,34 @@
 // To run the code:
 //     $ cargo run
 
-use derive_debug::CustomDebug;
-use std::fmt::Debug;
-
-pub trait Trait {
-    type Value;
-}
-
-#[derive(CustomDebug)]
-#[debug(bound = "T::Value: Debug")]
-pub struct Wrapper<T: Trait> {
-    field: Field<T>,
-}
-
-#[derive(CustomDebug)]
-struct Field<T: Trait> {
-    values: Vec<T::Value>,
-}
-
-fn assert_debug<F: Debug>() {}
+// use derive_debug::CustomDebug;
+// use std::fmt::Debug;
+//
+// pub trait Trait {
+//     type Value;
+// }
+//
+// #[derive(CustomDebug)]
+// #[debug(bound = "T::Value: Debug")]
+// pub struct Wrapper<T: Trait> {
+//     field: Field<T>,
+// }
+//
+// #[derive(CustomDebug)]
+// struct Field<T: Trait> {
+//     values: Vec<T::Value>,
+// }
+//
+// fn assert_debug<F: Debug>() {}
 
 fn main() {
-    struct Id;
-
-    impl Trait for Id {
-        type Value = u8;
-    }
-
-    assert_debug::<Wrapper<Id>>();
+    // struct Id;
+    //
+    // impl Trait for Id {
+    //     type Value = u8;
+    // }
+    //
+    // assert_debug::<Wrapper<Id>>();
 }
 
 
